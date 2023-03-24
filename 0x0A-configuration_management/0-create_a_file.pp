1a74918074471d4fbb2b32515e5eb5beb2d43245
Martian66 <kimmartin049@gmail.com>
2023-03-24 03:19:07 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-file { '/tmp' :
+# Using Puppet, create a file in /tmp
+
+file { '/tmp/school':
   ensure  => present,
-  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
