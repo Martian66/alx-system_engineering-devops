da0d341b4d8d416c8cb8181644fbb203346187d5
Kim Martin <110632901+Martian66@users.noreply.github.com>
2023-04-19 19:58:56 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# Create a manifest that kills a process named killmenow
-
-exec { 'killmenow':
-  command => '/usr/bin/pkill killmenow',
-  onlyif  => '/usr/bin/pgrep killmenow',
-  path    => '/usr/bin:/usr/sbin:bin:/sbin',
-}
